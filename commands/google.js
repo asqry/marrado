2e422b7bae0313d26aaf58c05790066e5490462b
seekeroftacos <38340485+seekeroftacos@users.noreply.github.com>
2018-05-22 16:38:59 -0500

    Update google.js

--- commands/google.js
+++ commands/google.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) => {
     let searchterm = args.join("%20");
     let searchurl = `https://www.google.com/search?q=${searchterm}&rlz=1C1CHBF_enUS781US781&oq=${searchterm}&aqs=chrome..69i57j0l5.1959j0j7&sourceid=chrome&ie=UTF-8`
     message.delete().catch();
-    message.channel.send(botmsg);
+    message.channel.send(searchurl);
 }
 
 module.exports.help = {
